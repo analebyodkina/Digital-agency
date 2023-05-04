8e4195da7872d4b9af897c2e7c09a6ff3dd1a391
analebyodkina <dp030992cad@gmail.com>
2023-05-04 15:26:05 +0300

    added portfolio section with owl carousel

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@ function navToggle() {
     var headerUl = document.getElementById("headerUl");
     headerUl.classList.toggle("active");
   }
+
+  $('.owl-carousel').owlCarousel({
+    // items:3,
+    lazyLoad:true,
+    loop:false,
+    margin:24,
+    responsiveClass:true,
+    responsive : {
+      // breakpoint from 0 up
+      0 : {
+          items:1
+      },
+      // breakpoint from 480 up
+      576 : {
+          items:2
+          
+      },
+      // breakpoint from 768 up
+      768 : {
+        items:3
+      }
+    }
+});
