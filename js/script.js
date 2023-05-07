b87195c5e1c05d08f2397082f968be1328492aa8
analebyodkina <dp030992cad@gmail.com>
2023-05-07 17:08:51 +0300

    made burger button special

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-// nav toogle
+// функция открывает бургер
 function navToggle() {
     var headerUl = document.getElementById("headerUl");
     headerUl.classList.toggle("active");
+    document.getElementById("html").classList.toggle("overflow-hidden");
+    document.getElementById("body").classList.toggle("overflow-hidden");
   }
 
-  $('.owl-carousel').owlCarousel({
-    // items:3,
+
+  // функция крутит кнопку бургера
+  $(document).ready(function(){
+    $(".header__burger").click(function(){
+      $(this).toggleClass("is-active");
+    });
+  });
+
+
+  // карусель
+  $('.owl-carousel').owlCarousel({    
     lazyLoad:true,
     loop:false,
     margin:24,
@@ -29,26 +40,30 @@ function navToggle() {
 
 
 // копирайт
-
 var currentYear = new Date().getFullYear();
 document.getElementById("footer-year").innerHTML = currentYear;
 
 
 // модальное окно
-
 const popUp = document.getElementById('popUp');
 function popup(){
       document.getElementById('headerUl').classList.remove("active");
+      document.getElementById("html").classList.add("overflow-hidden");
+      document.getElementById("body").classList.add("overflow-hidden");
       popUp.classList.add('active');
       return false;
 }
 // чтобы закрывалось окно при нажатии на черное
 popUp.addEventListener('click', function(){
     popUp.classList.remove('active');
+    document.getElementById("html").classList.remove("overflow-hidden");
+    document.getElementById("body").classList.remove("overflow-hidden");
 });
 const closePopUp = document.getElementById('closePopUp');
 closePopUp.addEventListener('click', () => {
     popUp.classList.remove('active');
+    document.getElementById("html").classList.remove("overflow-hidden");
+    document.getElementById("body").classList.remove("overflow-hidden");
 });
 
 popUp.querySelector('.pop-up__body').addEventListener('click', function(event){  
