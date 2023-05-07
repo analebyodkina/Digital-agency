09d59086a84831f71add05fbd0d76ea98fb04d72
analebyodkina <dp030992cad@gmail.com>
2023-05-07 16:12:27 +0300

    added fuction to open modal window

--- js/script.js
+++ js/script.js
@@ -27,5 +27,32 @@ function navToggle() {
     }
 });
 
+
+// копирайт
+
 var currentYear = new Date().getFullYear();
 document.getElementById("footer-year").innerHTML = currentYear;
+
+
+// модальное окно
+
+const popUp = document.getElementById('popUp');
+function popup(){
+      document.getElementById('headerUl').classList.remove("active");
+      popUp.classList.add('active');
+      return false;
+}
+// чтобы закрывалось окно при нажатии на черное
+popUp.addEventListener('click', function(){
+    popUp.classList.remove('active');
+});
+const closePopUp = document.getElementById('closePopUp');
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+});
+
+popUp.querySelector('.pop-up__body').addEventListener('click', function(event){  
+  event.stopPropagation();})
+
+
+// конец модального окна
